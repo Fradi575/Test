84e001ea7b2e8c58b07b2b5e4b4aba54afd85808
Fradi575 <78791893+Fradi575@users.noreply.github.com>
2022-05-14 14:13:29 +0200

    Sakktábla fenn
    Sakktábla fenn

---
+++ Models/Mezo.java
@@ -0,0 +1,17 @@
+package Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Mezo {
+
+    private int szin;
+    private int babu;
+}
+
+
